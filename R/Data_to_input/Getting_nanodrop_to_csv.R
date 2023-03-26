cca042d6a5596cd25b6b34eaa421c6040ace112e
fayweb <fy.webster@gmail.com>
2023-03-26 20:04:49 +0200

    nanodrop analysis

--- R/Data_to_input/Getting_nanodrop_to_csv.R
+++ R/Data_to_input/Getting_nanodrop_to_csv.R
@@ -7,18 +7,15 @@ library(plyr)
 library(readr)
 
 #read tsv table
-
-results <- read_tsv("Rodents_26032023.tsv")
+results <- read_tsv("CSV/Düppel_26022023.csv")
 
 # remove ffirst column
-results <-  results[,-c(1, 3)]
+results <-  results[,-1]
 
 #change the column names
-results <- results %>%
-    dplyr::rename("Sample" = "V2", "Date" = "V4", "Time" = "V5", "Nucleic_Acid" = "V6", 
-                  "Unit" = "V7", "A260" = "V8", "A280" = "V9", "260/280" = "V10", 
-                  "260/230" = "V11", "Sample_type" =  "V12", "Factor" = "V13" )
+
 write.csv(results, 
-          "~/GitHub/Namibia_project/Data/Nanodrop_measurements/CSV/Rodents_26032023.csv",
+          "~/GitHub/Namibia_project/Data/Nanodrop_measurements/CSV/Düppel_26022023.csv",
           row.names = FALSE)
+
 
